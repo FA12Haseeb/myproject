530ef1a208b1dff50e88829a82373470fb184f85
haseeb <wolfeyes764@gmail.com>
2016-01-14 10:57:05 +0500

    hello

---
+++ My/src/mycheck/Multiplication.java
@@ -0,0 +1,27 @@
+package mycheck;
+
+/**
+ * this is multiplication class
+ * @author Haseeb Ejaz
+ *
+ */
+public class Multiplication {
+/**
+ * this is multiplication method
+ * @param a
+ * @param b
+ * @return
+ */
+	
+	public int mul(int a, int b){
+		
+		
+		return a*b;
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ My/src/mycheck/Test.java
@@ -0,0 +1,22 @@
+package mycheck;
+
+/**
+ * this is test class
+ * @author Haseeb Ejaz
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Multiplication m= new Multiplication();
+		
+		//m.mul(2, 3);
+		
+		System.out.println(m.mul(2, 3));
+	}
+
+}
